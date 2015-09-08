512cbcabed65fe9329ceabcd7f07b4aca1877e79
Joel Dueck <joel@jdueck.net>
2015-09-08 14:18:33 -0500

    edits to milkweed poem notes, index entries

--- 02-milkweed.html.pm
+++ 02-milkweed.html.pm
@@ -4,7 +4,7 @@
 ◊(define-meta doc-publish-date "25/08/2015")
 ◊(define-meta author           "Richard Wilbur")
 
-◊margin-note{◊grey{◊newthought{Of the} two voices in this poem, the milkweed seems to have the better "voice": the sentiment is beautiful and the words have what you'd call a nice mouth-feel.  The stone's verse is ugly and awkward, maybe almost seeming like a simple case of bad writing. Wilbur made a deliberate choice to preserve this contrast. In ◊hyperlink["http://www.english.illinois.edu/maps/poets/s_z/wilbur/imageinterview.htm"]{an interview}, he said "the milkweed's speech is indeed written in one of my voices and was used for the sister's funeral in a genuine and appropriate way. But the other voice --- the 'slob' voice of the stone, is also one of my voices." }}
+◊margin-note{◊grey{◊newthought{Of the} two voices in this poem, the milkweed seems to have the better one: the sentiment is beautiful and the words have what you'd call a nice mouth-feel.  The stone's verse is ugly and awkward, maybe almost seeming like a simple case of bad writing. Whatever the verse's origin Wilbur chose to preserve this contrast. In ◊hyperlink["http://www.english.illinois.edu/maps/poets/s_z/wilbur/imageinterview.htm"]{an interview}, he said "the milkweed's speech is indeed written in one of my voices and was used for the sister's funeral in a genuine and appropriate way. But the other voice --- the 'slob' voice of the stone, is also one of my voices." }}
 
 ◊verse{
 ◊poem-heading{A Milkweed}
@@ -20,7 +20,7 @@ I shall possess the field.
 
 ◊poem-heading{A Stone}
 
-As casual as cow-dung
+As casual as ◊index-entry["excrement"]{cow-dung}
 Under the crib of God,
 I lie where chance would have me,
 Up to the ears in sod.
