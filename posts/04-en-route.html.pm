7b8a7e30c332c41f09c4f0119d0f5e619eaecfe8
Joel Dueck <joel@jdueck.net>
2015-09-20 11:09:45 -0500

    add En Route poem by Brander Matthews

---
+++ posts/04-en-route.html.pm
@@ -0,0 +1,78 @@
+#lang pollen
+
+◊(define-meta title            "En Route")
+◊(define-meta doc-publish-date "20/09/2015")
+◊(define-meta author           "J. Brander Matthews")
+◊(define-meta series           "series/poems.html")
+
+◊margin-note{◊grey{◊newthought{Each verse} consists of two lines from the verse before, and passes another two lines to the verse following. The resulting rhythm "really does remind one of the railway travel of the period", according to fellow poet Louella Styles Vincent, ◊hyperlink["https://books.google.com/books?id=Aek3AQAAMAAJ&lpg=PA266&ots=rf1SAfTR5E&dq=en%20route%20brander%20matthews&pg=PA266#v=onepage&q&f=false"]{writing in 1921} ('the period' being the 1870s). Still appropriate for recitation under one's breath during a morning commute. More notes in this ◊hyperlink["https://books.google.com/books?id=XOkGAQAAIAAJ&lpg=PR8&ots=O6Dx_Fsh0g&dq=%22en%20route%22%20brander%20matthews&pg=PA448#v=onepage&q&f=false"]{facsimile of the original}.}}
+
+◊verse{Here we are riding the rail,
+  Gliding from out of the station;
+Man though I am, I am pale,
+  Certain of heat and vexation.
+
+Gliding from out of the station,
+  Out from the city we thrust;
+Certain of heat and vexation,
+  Sure to be covered with dust.
+
+Out from the city we thrust,
+  Rattling we run o'er the bridges:
+Sure to be covered with dust,
+  Stung by a thousand of midges.
+
+Rattling we run o'er the bridges,
+  Rushing we dash o'er the plain;
+Stung by a thousand of midges,
+  Certain precursors of rain.
+
+Rushing we dash o'er the proclaiming
+  Watching the clouds darkly lowering,
+Certain precursors of rain:
+  Fields about here need a showering.
+
+Watching the clouds darkly lowering, ---
+  Track here is high on a bank ---
+Fields about here need a showering,
+  Boy with the books needs a spank.
+
+Track here is high on a bank,
+  Just by a wretched old hovel:
+Boy with the books needs a spank ---
+  "No! I don't want a new novel!"
+
+Just by a wretched old hovel,
+  Small speck of dust in my eye.
+"No! I don't want a new novel!"
+  --- Babies beginning to cry. ---
+
+Small speck of dust in my eye,
+  "I will not buy papers or candy!"
+--- Babies beginning to cry ---
+  Oh, for a tomahawk handy!
+
+"I will not buy papers or candy!"
+  Train boys deserve to be slain;
+Oh, for a tomahawk handy!
+  Oh, for the cool of the rain!
+
+Train boys deserve to be slain,
+  Heat and the dust --- they are choking,
+Oh, for the cool of the rain!
+  --- "Gent" just behind me is joking.
+
+Heat and the dust they are joking,
+  Clogging and filling my pores,
+--- "Gent" just behind me is joking,
+"Gent" just in front of me snores.
+
+Clogging and filling my pores,
+  Ears are on edge at the rattle;
+"Gent" just in front of me snores,
+  Sounds like the noise of a battle.
+
+Ears are on edge at the rattle,
+  Man though I am, I am pale,
+Sounds like the noise of a battle,
+  Here we are riding the rail.}
