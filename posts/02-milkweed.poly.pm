a71d2d4f682e5b1b0ac956efa5d370bb5b53a3b5
Joel Dueck <joel@jdueck.net>
2015-12-20 10:57:24 -0600

    Move more index entries out of poems and into margin notes

--- posts/02-milkweed.poly.pm
+++ posts/02-milkweed.poly.pm
@@ -5,7 +5,9 @@
 ◊(define-meta author           "Richard Wilbur")
 ◊(define-meta series           "series/poems.html")
 
-◊margin-note{◊grey{◊newthought{Of the} two voices in this poem, the milkweed seems to have the better one: the sentiment is beautiful and the words have what you'd call a nice mouth-feel.  The stone's verse is ugly and awkward, maybe almost seeming like a simple case of bad writing. Whatever the verse's origin Wilbur chose to preserve this contrast. In ◊hyperlink["http://www.english.illinois.edu/maps/poets/s_z/wilbur/imageinterview.htm"]{an interview}, he said "the milkweed's speech is indeed written in one of my voices and was used for the sister's funeral in a genuine and appropriate way. But the other voice --- the 'slob' voice of the stone, is also one of my voices." }}
+◊margin-note{◊grey{◊newthought{Of the} two voices in this poem, the milkweed seems to have the better one: the sentiment is beautiful and the words have what you'd call a nice mouth-feel.  The stone's verse is ugly and awkward, maybe almost seeming like a simple case of bad writing. Whatever the verse's origin Wilbur chose to preserve this contrast. In ◊hyperlink["http://www.english.illinois.edu/maps/poets/s_z/wilbur/imageinterview.htm"]{an interview}, he said "the milkweed's speech is indeed written in one of my voices and was used for the sister's funeral in a genuine and appropriate way. But the other voice --- the 'slob' voice of the stone, is also one of my voices."}
+    ◊index-entry["excrement"]{}
+}
 
 ◊verse[#:title "A Milkweed"]{
 Anonymous as cherubs
@@ -18,7 +20,7 @@ Shatter me, great wind:
 I shall possess the field.}
 
 ◊verse[#:title "A Stone"]{
-As casual as ◊index-entry["excrement"]{cow-dung}
+As casual as cow-dung
 Under the crib of God,
 I lie where chance would have me,
 Up to the ears in sod.
