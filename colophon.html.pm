0bfe95e8ae3f6008c4374b3afe408ea8e44ef7c5
Joel Dueck <joel@jdueck.net>
2016-01-04 22:48:33 -0600

    Update colophon to reflect new PDF powers

--- colophon.html.pm
+++ colophon.html.pm
@@ -1,12 +1,14 @@
 #lang pollen
 
 ◊(define-meta title            "Colophon")
-◊(define-meta doc-publish-date "2015-08-25")
+◊(define-meta doc-publish-date "2016-01-04")
 ◊(define-meta author           "Joel Dueck")
 
-These pages are my playground for trying out ◊hyperlink["http://pollenpub.com"]{Pollen}, a system for publishing books on the web or anywhere else. The Pollen source is available ◊hyperlink["https://github.com/otherjoel/try-pollen"]{on Github}.
+These pages are my playground for trying out ◊hyperlink["http://pollenpub.com"]{Pollen}, a system for publishing books on the web or anywhere else.
 
-The CSS design comes from the ◊hyperlink["http://www.daveliepmann.com/tufte-css/"]{Tufte CSS} project.
+In particular, I’m trying to experiment with marrying web documents with printed books: generating high quality print-ready ◊smallcaps{PDF} files from the same markup used to create the web pages. The ◊hyperlink["http://www.daveliepmann.com/tufte-css/"]{Tufte ◊smallcaps{CSS}} project, and the ◊hyperlink["https://tufte-latex.github.io/tufte-latex/"]{Tufte-LaTeX} project on which it is based, turned out to be the perfect tools for the job. On most pages (including this one!) you can click the ◊smallcaps{PDF} link to see the LaTeX-generated print version.
+
+The Pollen source is available ◊hyperlink["https://github.com/otherjoel/try-pollen"]{on Github}.
 
 I'm ◊hyperlink["http://twitter.com/joeld"]{on Twitter}. You can also find me at my ◊hyperlink["http://tilde.club/~joeld"]{tilde.club page} and my main site, ◊hyperlink["http://thelocalyarn.com"]{◊i{The Local Yarn}}, neither of which (yet) use Pollen.
 
