b69c7fa250c457553035e8d89453ec278901d18b
Joel Dueck <joel@jdueck.net>
2016-01-17 16:09:57 -0600

    Update colophon

--- colophon.poly.pm
+++ colophon.poly.pm
@@ -6,10 +6,10 @@
 
 These pages are my playground for trying out ◊hyperlink["http://pollenpub.com"]{Pollen}, a system for publishing books on the web or anywhere else.
 
-In particular, I’m trying to experiment with marrying web documents with printed books: generating high quality print-ready ◊smallcaps{PDF} files from the same markup used to create the web pages. The ◊hyperlink["http://www.daveliepmann.com/tufte-css/"]{Tufte ◊smallcaps{CSS}} project, and the ◊hyperlink["https://tufte-latex.github.io/tufte-latex/"]{Tufte-LaTeX} project on which it is based, turned out to be the perfect tools for the job. On most pages (including this one!) you can click the ◊smallcaps{PDF} link to see the LaTeX-generated print version.
+In particular, I’m trying to experiment with marrying web documents with printed books: generating high quality print-ready PDF files from the same markup used to create the web pages. The ◊hyperlink["http://www.daveliepmann.com/tufte-css/"]{Tufte ◊smallcaps{CSS}} project, and the ◊hyperlink["https://tufte-latex.github.io/tufte-latex/"]{Tufte-◊Latex[]} project on which it is based, turned out to be the perfect tools for the job.
 
-The Pollen source is available ◊hyperlink["https://github.com/otherjoel/try-pollen"]{on Github}.
+On most pages (including this one!) you can click the PDF link to see the ◊Latex[]-generated print version of that page. The home page also includes a PDF of the complete book ◊i{Flatland}. Again, the point of interest here isn’t that each page has a corresponding PDF, but that the same text files generate both the website and the PDF files, using a custom, light-weight markup that can take full advantage of both HTML and ◊Latex[].
+
+The Pollen source code for this site is available ◊hyperlink["https://github.com/otherjoel/try-pollen"]{on Github}.
 
 I'm ◊hyperlink["http://twitter.com/joeld"]{on Twitter}. You can also find me at my ◊hyperlink["http://tilde.club/~joeld"]{tilde.club page} and my main site, ◊hyperlink["http://thelocalyarn.com"]{◊i{The Local Yarn}}, neither of which (yet) use Pollen.
-
-Please excuse my freshman writing.
