c1fb728e5182dda08e24e82f552562d0f48cf7c9
Joel Dueck <joel@jdueck.net>
2016-02-21 00:43:08 -0600

    Add xref link in colophon

--- colophon.poly.pm
+++ colophon.poly.pm
@@ -6,7 +6,7 @@
 
 These pages are my playground for trying out ◊hyperlink["http://pollenpub.com"]{Pollen}, a system for publishing books on the web or anywhere else.
 
-In particular, I’m trying to experiment with marrying web documents with printed books: generating high quality print-ready PDF files from the same markup used to create the web pages. The ◊hyperlink["http://www.daveliepmann.com/tufte-css/"]{Tufte ◊smallcaps{CSS}} project, and the ◊hyperlink["https://tufte-latex.github.io/tufte-latex/"]{Tufte-◊Latex[]} project on which it is based, turned out to be the perfect tools for the job.
+In particular, I’m trying to experiment with ◊hyperlink["/posts/web-books.html"]{marrying web documents with printed books}: generating high quality print-ready PDF files from the same markup used to create the web pages. The ◊hyperlink["http://www.daveliepmann.com/tufte-css/"]{Tufte ◊smallcaps{CSS}} project, and the ◊hyperlink["https://tufte-latex.github.io/tufte-latex/"]{Tufte-◊Latex[]} project on which it is based, turned out to be the perfect tools for the job.
 
 On most pages (including this one!) you can click the PDF link to see the ◊Latex[]-generated print version of that page. The home page also includes a PDF of the complete book ◊i{Flatland}. Again, the point of interest here isn’t that each page has a corresponding PDF, but that the same text files generate both the website and the PDF files, using a custom, light-weight markup that can take full advantage of both HTML and ◊Latex[].
 
