e0e9c3f1b91065fecb91d17497a56693a1f3a962
Joel Dueck <joel@jdueck.net>
2016-07-06 22:53:09 -0500

    Update pandoc template
    Any markdown-formatted drafts will use "date" instead of "published"
    for the date field, since they are drafts calling it "published" is
    premature.

--- util/pandoc-pollen-template.pm
+++ util/pandoc-pollen-template.pm
@@ -1,7 +1,7 @@
 #lang pollen
 
 ◊(define-meta title "$title$")
-◊(define-meta doc-publish-date "$published$")
+◊(define-meta doc-publish-date "$date$")
 $if(tags)$◊(define-meta tags "$tags$")$endif$
 
 $body$
